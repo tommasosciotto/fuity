42219d59e9179bd3509606caaafb64ee7e6d730c
tommasosciotto <tommaso.sciotto@gmail.com>
2019-02-11 03:49:16 +0100

    Update Intro.js

--- Intro.js
+++ Intro.js
@@ -1,8 +1,19 @@
+/*
+  Tommaso Sciotto
+  10325866/914844
+    "Fuity"
+  Advanced Coding Tools and Methodologies
+  Computer Music - Representations and Models
+  06/02/2019
+*/
+
+//NOTE: this file is only used as the source for the narrator voice in the trailer.
+
 var voices = window.speechSynthesis.getVoices();
 var utterance = new SpeechSynthesisUtterance();
 utterance.voice = voices[6];
 
-utterance.text = "Such careless attitude attracts an individual of your kind.";
+utterance.text = "";
 speechSynthesis.speak(utterance);
 
 /*
@@ -13,17 +24,4 @@ speechSynthesis.speak(utterance);
 "Perhaps they mock you, perhaps they show some interest."
 "If you take the right steps, and you learn to listen, and you are consistent, and you don't get boring,"
 "Whatever game it is, you're going to make this a game for two."
-*/
-
-
-/*
-
-"You are a parrot, all by yourself in some sort of peaceful, eerie wilderness."
-"Silence is thick and you burst into singing, although it seems you've never tried before."
-"Such careless attitude attracts an individual of your kind."
-"How should they respond? As any parrot would: by imitating."
-"Perhaps they mock you, perhaps they show some interest.""
-"And if you take the right steps, and you learn to listen, and you try being consistent, and you don't get boring,"
-"You're going to make this a game for two."
-
 */
